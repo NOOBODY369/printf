88fababc2e0191d9e8184bfd1c3a2d1af74c831d
sami <sami-karem@outlook.fr>
2023-07-26 01:42:10 +0100

    printf

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ unsigned int	_putlowhex(unsigned int nbr, int *len);
 unsigned int	_putuphex(unsigned int nbr, int *len);
 unsigned int	_putd(unsigned int nb, int *len);
 int				_putnbr(int nb, int *len);
-void			_putstr(char *str);
+int			_putstr(char *str);
 unsigned long	_putaddr(unsigned long nbr, int *len);
 void			_check(char *s, va_list valist, int *len, int i);
 int				_printf(const char *format, ...);
